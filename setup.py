3a15077a995f8803e20d6214a24015f93f890e26
AbodyPrmaga <abdelrahmanmbartak@gmail.com>
2025-07-29 01:20:45 +0300

    Update setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="klang",
+    version="0.1.0",
+    description="Get current keyboard layout language (Windows only)",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Abdelrahman",
+    author_email="abdelrahmanmbartak@gmail.com",
+    url="https://github.com/AbodyPrmaga/klang",  # اختياري لو عندك مستودع GitHub
+    packages=find_packages(),
+    install_requires=["pywin32","ctypes","win32process"],  # مكتبة win32 ضرورية
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: Microsoft :: Windows",
+        "License :: OSI Approved :: MIT License"
+    ],
+    license="MIT",
+)
